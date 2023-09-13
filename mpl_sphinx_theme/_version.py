0dd03fefdae592e61d830c27362684ddf1422d6b
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2023-09-13 16:48:57 -0400

    REL: 3.8.0
    Bump version to 3.8.0 final.

--- mpl_sphinx_theme/_version.py
+++ mpl_sphinx_theme/_version.py
@@ -5,4 +5,4 @@
 # Distributed under the terms of the Modified BSD License.
 
 version_info = (3, 8, 0)
-__version__ = ".".join(map(str, version_info)) + "rc1"
+__version__ = ".".join(map(str, version_info))
